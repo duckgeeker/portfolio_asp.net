d4fa190eda7684c430967cd0dee22d147ff7fcd5
duckgeeker <57014664+duckgeeker@users.noreply.github.com>
2020-08-10 18:57:50 +0300

    Add files via upload

---
+++ App_Data/SampleData.cs
@@ -0,0 +1,34 @@
+﻿using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.App_Data
+{
+    public class SampleData
+    {
+        public static void Initialize(PortfolioContext context)
+        {
+            if (!context.Works.Any())
+            {
+                context.Works.AddRange
+                (
+                    new Work
+                    {
+                        desc_short = "TEST: Banner for up2date",
+                        desc_long = "",
+                        tools = "Adobe Photoshop"
+                    },                    
+                    new Work
+                    {
+                        desc_short = "TEST: Banner for RomanD",
+                        desc_long = "",
+                        tools = "Adobe XD"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
